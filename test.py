0c9239ffa11d037e8ce0e60c18f84b36899bbdd3
Radomir Fugiel <radek@Radomirs-MacBook-Pro.local>
2020-08-13 20:31:43 -0500

    changes

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 print("hello world this is Rad")
-=======
-print("hello world dan sucks")
->>>>>>> 512e9e1115e0de660d507e0221eb7893adcd84c1
-
 
 def test():
 	print("This is a test and more")
@@ -11,9 +6,5 @@ def test():
 		"Alpha": 1,
 		"Beta": 2,
 	}
-<<<<<<< HEAD
 
 	print("some stuff")
-=======
-	print("more stuff")
->>>>>>> 512e9e1115e0de660d507e0221eb7893adcd84c1
