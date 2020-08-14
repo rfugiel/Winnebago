512e9e1115e0de660d507e0221eb7893adcd84c1
Radomir Fugiel <radek@Radomirs-MacBook-Pro.local>
2020-08-13 20:26:55 -0500

    changes

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-print("hello world")
+print("hello world dan sucks")
 
 
 def test():
-	print("This is a test")
+	print("This is a test and more")
 	x = {
 		"Alpha": 1,
 		"Beta": 2,
 	}
+	print("more stuff")
